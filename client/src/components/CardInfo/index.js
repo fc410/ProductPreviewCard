ce8ab6a99fea739fffc1592d7dbb66a483cdfc71
Fernando Crespo <54721672+fc410@users.noreply.github.com>
2022-08-27 20:00:09 -0700

    CardInfo tags
    Completed all the information needed for the cardInfo components and added all the tags just need to style it now.

--- client/src/components/CardInfo/index.js
+++ client/src/components/CardInfo/index.js
@@ -1,9 +1,34 @@
-// import '../../App.css';
+import ProductImage from '../Assets/image-product-desktop.jpg';
+import Cart from '../Assets/icon-cart.svg';
 
 const CardInfo = () => {
     return(
         <div className="card-main-content">
-            Card Page
+            <img src={ProductImage} alt='product' className='product-img'/>
+            <div className='card-info'>
+                <p>
+                    Perfume
+                </p>
+                <p>
+                    Gabriel Essence Eau De Parfum
+                </p>
+                <p>
+                    A floral, solar and voluptuous interpretation composed by 
+                    Olivier Polge, Perfumer-Creator for the House of CHANEL.
+                </p>
+                <div className='product-prices'>
+                    <p>
+                        $149.99
+                    </p>
+                    <p>
+                        $169.99
+                    </p>
+                </div>
+
+                <button className='info-button'>
+                    <img src={Cart} alt='cart' className='cart-img'/> Add to cart
+                </button>
+            </div>
         </div>
     );
 }
